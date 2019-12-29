f0d7a938e12b1cc1cd2bce2f5bf63ce696b39ad2
hairongwang <wanghr0402@gmail.com>
2019-12-30 00:34:19 +0800

    Update typed.js

--- js/typed.js
+++ js/typed.js
@@ -436,3 +436,12 @@
 
 
 }(window.jQuery);
+
+$("#changerificwordspanid").typed({
+    strings: ["good", "happy", "healthy", "tall"],
+    typeSpeed: 100,
+    startDelay: 10,
+    showCursor: true,
+    shuffle: true,
+    loop:true
+});
